c51f58b4547af1a8ba52761678fffaa8fab03338
JuanDC10 <juancastpar@unisabana.edu.co>
2026-03-12 13:35:04 -0500

    1.8

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
-def promedio_estudiantes(calificaciones):
+def promedio_estudiante(calificaciones):
     if len(calificaciones) == 0:
         return 0.0
     return sum(calificaciones) / len(calificaciones)
-calificaciones = []
