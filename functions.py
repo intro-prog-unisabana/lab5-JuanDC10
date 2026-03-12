9b1af4bd45760bedffc29f66147fcce7354df815
JuanDC10 <juancastpar@unisabana.edu.co>
2026-03-12 13:23:07 -0500

    1.4

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
-def promedio_estudiantes (números):
-    if len (números) == 0:
+def promedio_estudiantes(números):
+    if len(números) == 0:
         return 0.0
     return sum(números) / len(números)
 
