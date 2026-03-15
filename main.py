451c44a5290dcaa6f950751af315d9789d04890d
JuanDC10 <juancastpar@unisabana.edu.co>
2026-03-15 16:08:24 -0500

    10.1

---
+++ main.py
@@ -0,0 +1,10 @@
+def add(num1, num2):
+    return num1 + num2
+def sub(num1, num2):
+    return num1 - num2
+def multiply(num1, num2):
+    return num1 * num2
+def divide(num1, num2):
+    if num2 == 0:
+        return "Error: Division by zero is not allowed."
+    return num1 / num2
