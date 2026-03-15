8c8330af1c5172c03cb751ffc226236e130040a3
JuanDC10 <juancastpar@unisabana.edu.co>
2026-03-15 16:12:41 -0500

    10.4

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
-def add(num1, num2):
-    return num1 + num2
-def sub(num1, num2):
-    return num1 - num2
-def multiply(num1, num2):
-    return num1 * num2
-def divide(num1, num2):
-    if num2 == 0:
-        return "Error: Division by zero is not allowed."
-    return num1 / num2
+from utils import *
+while True:
+    operation = input(
+        "Which calculation would you like to perform? (add, subtract, multiply, divide, exponent, modulo, floor_divide, absolute, exit):\n"
+    ).lower()
+    if operation == "exit":
+        break
+    elif operation == "add":
+        num1 = float(input("Enter the first number:\n"))
+        num2 = float(input("Enter the second number:\n"))
+        print("The result is:", add(num1, num2))
+    elif operation == "subtract":
+        num1 = float(input("Enter the first number:\n"))
+        num2 = float(input("Enter the second number:\n"))
+        print("The result is:", sub(num1, num2))
+    elif operation == "multiply":
+        num1 = float(input("Enter the first number:\n"))
+        num2 = float(input("Enter the second number:\n"))
+        print("The result is:", multiply(num1, num2))
+    elif operation == "divide":
+        num1 = float(input("Enter the first number:\n"))
+        num2 = float(input("Enter the second number:\n"))
+        result = divide(num1, num2)
+        print(result if isinstance(result, str) else "The result is: " + str(result))
+    elif operation == "exponent":
+        num1 = float(input("Enter the first number:\n"))
+        num2 = float(input("Enter the second number:\n"))
+        print("The result is:", exponent(num1, num2))
+    elif operation == "modulo":
+        num1 = float(input("Enter the first number:\n"))
+        num2 = float(input("Enter the second number:\n"))
+        result = modulo(num1, num2)
+        print(result if isinstance(result, str) else "The result is: " + str(result))
+    elif operation == "floor_divide":
+        num1 = float(input("Enter the first number:\n"))
+        num2 = float(input("Enter the second number:\n"))
+        result = floor_divide(num1, num2)
+        print(result if isinstance(result, str) else "The result is: " + str(result))
+    elif operation == "absolute":
+        num = float(input("Enter the number:\n"))
+        print("The result is:", absolute(num))
+    else:
+        print("Invalid option!")
