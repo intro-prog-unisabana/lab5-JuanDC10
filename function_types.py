970843d6f4752ee9b101cfcf7b4bc191575cd249
JuanDC10 <juancastpar@unisabana.edu.co>
2026-03-15 11:45:40 -0500

    3.6

--- function_types.py
+++ function_types.py
@@ -1,9 +1,11 @@
 def list_shift (lista, valor):
     for i in range (len(lista)):
         lista[i] = lista[i] + valor
-def calc_avg (lista):
-    sum = 0
+def calc_avg(lista):
+    suma = 0
     for num in lista:
-        suma = num + sum
-        promedio = suma / len(lista)
+        suma = suma + num
+    promedio = suma / len(lista)
     return promedio
+def print_normalized(lista):
+    print(lista)
