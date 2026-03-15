3eed8e74110e88ab07bd742076cacf3d6cdd06fe
JuanDC10 <juancastpar@unisabana.edu.co>
2026-03-15 14:15:42 -0500

    5.3

--- modules.py
+++ modules.py
@@ -2,4 +2,8 @@ import os
 import math
 cwd = os.getcwd()
 print(f"Current working directory: {cwd}")
-
+num = int(input("Enter an integer: "))
+log_val = math.log2(num)
+print(f"Log base 2 of {num} is: {log_val}")
+print(f"Floor: {math.floor(log_val)}")
+print(f"Ceiling: {math.ceil(log_val)}")
