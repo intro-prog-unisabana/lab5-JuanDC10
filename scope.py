6fe7d32dc5c113afb2ee041519a5259991b6116d
JuanDC10 <juancastpar@unisabana.edu.co>
2026-03-15 11:57:15 -0500

    3.1

--- scope.py
+++ scope.py
@@ -1,6 +1,4 @@
 def set_globals (some_int, some_str):
-    global int_value
-    global str_value
-    int_value = some_int
-    str_value = some_str
-    
+    global some_int
+    global some_str
+
