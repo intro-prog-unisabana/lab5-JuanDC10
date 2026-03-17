57a542c2746f737acdd70e1ca7133a6ce3affe2a
JuanDC10 <juancastpar@unisabana.edu.co>
2026-03-17 18:36:41 -0500

    13.1

--- mystery_module.py
+++ mystery_module.py
@@ -1,5 +1,7 @@
+from mystery_module import transform_data
 # FREEZE CODE BEGIN
-def transform_data(x: int, y: float, z: str) -> float:
-    result = (x * y) / (len(z) + 1) + sum(ord(c) for c in z) % 10
-    return round(result, 2)
+x = int(input())
+y = float(input())
 # FREEZE CODE END
+a = transform_data(x,y,"quiz_test")
+print(a)
